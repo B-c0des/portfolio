ca3f242499d44afec149f0a23c26cf3202fccb23
Bryan Reed <brian.reed.software@outlook.com>
2022-02-24 15:26:44 -0800

    updated styling

--- src/components/GithubProfileCard.jsx
+++ src/components/GithubProfileCard.jsx
@@ -10,11 +10,11 @@ import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 import Resume from "../components/Resume";
 const GithubProfileCard = ({ prof }) => {
   const useStyles = makeStyles((theme) => ({
-      card: {
-   background: 'linear-gradient(80deg, steelblue 10%,rgb(106, 206, 206) 80%, #80e5ff 110%)',
-    boxShadow: `inset 0px 0px 50px 2px black,inset 0px 0px 5px 5px black, 0 0 20px -5px black;`,
-   borderBottom: '1px solid white',
-    borderRight: '2px solid gray',
+    card: {
+      background: 'linear-gradient(80deg, steelblue 10%,rgb(106, 206, 206) 80%, #80e5ff 110%)',
+      boxShadow: `inset 0px 0px 50px 2px black,inset 0px 0px 5px 5px black, 0 0 20px -5px black;`,
+      borderBottom: '1px solid white',
+      borderRight: '2px solid gray',
     '&:hover': {
       cursor: 'pointer'},
       border: 0,
@@ -27,34 +27,34 @@ const GithubProfileCard = ({ prof }) => {
       borderRadius: '10px',
       height: '60%',
      },
-     glowingCard:{
-       filter: "drop-shadow(0px 0px 50px steelblue)",
-       justifyContent: "center"
+    glowingCard:{
+      filter: "drop-shadow(0px 0px 50px steelblue)",
+      justifyContent: "center"
      },
-     avatar: {
+    avatar: {
       width: "50%",
       border: "2px solid white",
       filter: "drop-shadow(0px 0px 20px white)",
       borderRadius: "100%",
      },
-     messageForm: {
+    messageForm: {
       width: "100%",
      },
-     textName: {
-       display: "flex",
-       justifyContent: "center",
-       color: "white",
-       filter: "drop-shadow(0px 0px 25px white)",
+    textName: {
+      display: "flex",
+      justifyContent: "center",
+      color: "white",
+      filter: "drop-shadow(0px 0px 25px white)",
     },
-         subHeader:{
-    border: 0,
-    color: "white",
-    padding: '1px',
-    margin:'3px',
-    textShadow: '0 2px 5px white',
-    textAlign: 'center',
-    userSelect: "none",
-    borderRadius: '10px',
+    subHeader:{
+      border: 0,
+      color: "white",
+      padding: '1px',
+      margin:'3px',
+      textShadow: '0 2px 5px white',
+      textAlign: 'center',
+      userSelect: "none",
+      borderRadius: '10px',
      },
    }));
   
@@ -67,9 +67,9 @@ const GithubProfileCard = ({ prof }) => {
        <div className="p-2" style={{filter: "drop-shadow(0px 0px 10px black)"}}>
        <Row className="justify-content-center">
         <Col className={classes.card} lg="4">
-                <img className={classes.avatar} src={prof.avatar_url} alt="" />
-                <h5 className="text-white">{'brian.reed.software@outlook.com'}</h5>
-          <h3 className="" style={{filter: 'drop-shadow(0px 0px 5px black )' , textAlign: 'center'}}>
+           <img className={classes.avatar} src={prof.avatar_url} alt="" />
+           <h5 className="text-white">{'brian.reed.software@outlook.com'}</h5>
+           <h3 className="" style={{filter: 'drop-shadow(0px 0px 5px black )' , textAlign: 'center'}}>
            <span className={classes.subHeader}>{prof.name}</span>
            <br/>
            <h3 className={classes.subHeader}>{prof.bio}</h3>
@@ -83,8 +83,8 @@ const GithubProfileCard = ({ prof }) => {
           </Col>
            <Col lg="8" className={classes.messageForm} >
            <h2 id="contact" className={classes.textName}>
-            <h5 className="text-info">{'brian.reed.software@outlook.com'}</h5>
-                </h2>
+           <h5 className="text-info">{'brian.reed.software@outlook.com'}</h5>
+           </h2>
                 
           {/* <div data-netlify-recaptcha="true" data-netlify="true" class="mighty-form" id="mf-e9b1f4b8-0f47-41c1-a114-0c1eac9921d5" ></div>  */}
           </Col>                    
